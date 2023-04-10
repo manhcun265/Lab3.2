6b28e201b3d6ea91419eeaad1ea1e405b3bc42a1
manhcun265 <manhcun265@gmail.com>
2023-04-10 23:36:23 +0700

    baitap

---
+++ Test.java
@@ -0,0 +1,17 @@
+public class Test {
+    public static void main(String[] args) {
+    System.out.println("Tam giac: ");
+	Tamgiac tg= new Tamgiac(0, 0);
+	tg.nhapTG();
+	tg.inTG();
+    tg.KiemTra();
+	tg.tinhChuVi();
+	tg.tinhDienTich();
+
+	System.out.println("Duong tron: ");
+	DuongTron dt= new DuongTron(0, 0, 0);
+	dt.nhapDT();
+	System.out.println("Chu vi duong tron: "+dt.chuViDT());
+	System.out.println("Dien tich duong tron: "+dt.dienTichDT());
+}
+    }
